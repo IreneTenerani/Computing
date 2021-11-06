2217b71b082510ad5f25f63e70b0ff91f2a07348
Maria Irene Tenerani <m.tenerani2@studenti.unipi.it>
2021-11-06 12:40:03 +0100

    non mi faceva fare il pull altrimenti

--- Assignment_5/test_Assegnamento5.py
+++ Assignment_5/test_Assegnamento5.py
@@ -8,50 +8,49 @@ class TestVoltageData(unittest.TestCase): #- [optional] rewrite the run_tests() 
 
 
     def setUp(self):
-        data2=VoltageData.from_file('Sample_data_file.txt') #Non ci interessa che data sia  un oggetto della classe unitest, deve solo essere della classe voltagedata
+        self.data2=VoltageData.from_file('Sample_data_file.txt')
         self.t, self.v = np.loadtxt('Sample_data_file.txt', unpack=True)
-        data =VoltageData(self.t , self.v)
+        self.data =VoltageData(self.t , self.v)
 
     def test_len(self):
         self.assertEqual( len(self.t) , len(self.v) )
 
     def test_attribute(self):
-        self.assertTrue(np.array_equal(data.voltages, self.v, equal_nan=True))
-        self.assertTrue(np.array_equal(data.time, self.t, equal_nan=True))
+        self.assertTrue(np.array_equal(self.data.voltages, self.v, equal_nan=True))
+        self.assertTrue(np.array_equal(self.data.time, self.t, equal_nan=True))
 
     def test_square_parenthesis(self):
-        self.assertAlmostEqual(self.v[3], data.voltages(3, 1)) #Così potrebbe funzionare, getitem deve avere solo un valore in ingresso perchè così è definito il metodo speciale __getitem__
-        self.assertAlmostEqual(self.t[-1], data.__getitem__(-1, 0))
+        self.assertAlmostEqual(self.v[3], self.data.voltages[3]) #Così potrebbe funzionare, getitem deve avere solo un valore in ingresso perchè così è definito il metodo speciale __getitem__
+        self.assertAlmostEqual(self.t[-1], self.data.time[-1])
 
-    '''def test_slicing(self):
-        for i in range(5):
-            self.assertTrue( self.v[i] , self.data.__getitem__(i, 1) ) #(v_data[1:5, 1] == v[1:5]) 
-            Secondo me è self.v[1:5]=data.voltages[1:5]
-    '''
+    def test_slicing(self):
+        self.assertTrue(np.array_equal(self.v[1:5], self.data.voltages[1:5], equal_nan=True))
+
+
     def test_constructor_from_data_file(self):
-    
-        self.assertTrue(np.array_equal( data2.voltages, self.v , equal_nan=True))
-        self.assertTrue(np.array_equal( data2.time, self.t , equal_nan=True))
+
+        self.assertTrue(np.array_equal( self.data2.voltages, self.v , equal_nan=True))
+        self.assertTrue(np.array_equal( self.data2.time, self.t , equal_nan=True))
 
     def test_iteration(self):
-        for i, entry in enumerate(data):
+        for i, entry in enumerate(self.data):
             self.assertAlmostEqual( entry[1], self.v[i])
             self.assertAlmostEqual( entry[0], self.t[i])
 
     def test_interpolation(self):
-        v5=data(data.time[5])
+        v5=self.data(self.data.time[5])
         self.assertTrue(np.abs(self.v[5]- v5< 1.e-5))
 
     def test_plot(self):
-        data.plot(fmt='ko', markersize=6, label='normal voltage')
+        self.data.plot(fmt='ko', markersize=6, label='normal voltage')
         x_grid = np.linspace(min(self.t), max(self.t), 200)
-        plt.plot(x_grid, data(x_grid), 'r-', label='spline')
+        plt.plot(x_grid, self.data(x_grid), 'r-', label='spline')
         plt.legend()
         plt.show()
 
     def test_print(self):
-        print(data,'\n')
-        print(repr(data))
+        print(self.data,'\n')
+        print(repr(self.data))
 
 
 if __name__=='__main__':
